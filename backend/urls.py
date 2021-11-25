ab10a35424f3b581771d01adfa0ac8c40cf70767
Ubuntu <ubuntu@ip-172-31-33-227.ap-northeast-2.compute.internal>
2021-11-25 05:46:58 +0000

    Changed in server

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('community/', include('community.urls')),
     path('movies/', include('movies.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
